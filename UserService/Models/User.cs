ac920ac9fe0b15e1e8f3ba98bb7e52c75434655d
Braunerr <mathn519@gmail.com>
2023-05-23 08:11:42 +0200

    commented out errors

--- UserService/Models/User.cs
+++ UserService/Models/User.cs
@@ -1,7 +1,8 @@
-using MongoDB.Bson.Serialization.Attributes;
+//using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
 
+// tilføj package og lav model til lister
 public class User
 {
     public Guid Id { get; set; }
@@ -9,11 +10,10 @@ public class User
     public String LastName { get; set; }
     public String Email { get; set; }
     public Int32 MobilNummer { get; set; }
-    public List<Bid> BidHistory { get; set; }
-    public List<ImageRecord> ImageHistory { get; set; }
+
+    //public List<Bid> BidHistory { get; set; }
+    //public List<ImageRecord> ImageHistory { get; set; }
 
     public string Category { get; set; }
     public string Location { get; set; }
-
-
 }
