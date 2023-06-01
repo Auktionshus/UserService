0a183df1f866686e069db0b55cf4b1dfecc1ea54
Braunerr <mathn519@gmail.com>
2023-06-01 11:05:41 +0200

    vault

--- UserService/Program.cs
+++ UserService/Program.cs
@@ -4,13 +4,58 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using NLog;
 using NLog.Web;
+using VaultSharp;
+using VaultSharp.V1.AuthMethods.Token;
+using VaultSharp.V1.AuthMethods;
+using VaultSharp.V1.Commons;
 
 var logger = NLog.LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
 logger.Debug("init main");
 
 try
 {
+    var EndPoint = "https://vault_dev:8201/";
+    logger.Info($"EndPoint: {EndPoint}");
+    var httpClientHandler = new HttpClientHandler();
+    httpClientHandler.ServerCertificateCustomValidationCallback = (
+        message,
+        cert,
+        chain,
+        sslPolicyErrors
+    ) =>
+    {
+        return true;
+    };
+
+    // Initialize one of the several auth methods.
+    IAuthMethodInfo authMethod = new TokenAuthMethodInfo("00000000-0000-0000-0000-000000000000");
+    // Initialize settings. You can also set proxies, custom delegates etc. here.
+    var vaultClientSettings = new VaultClientSettings(EndPoint, authMethod)
+    {
+        Namespace = "",
+        MyHttpClientProviderFunc = handler =>
+            new HttpClient(httpClientHandler) { BaseAddress = new Uri(EndPoint) }
+    };
+    IVaultClient vaultClient = new VaultClient(vaultClientSettings);
+    logger.Info($"vault client created: {vaultClient}");
+    // Use client to read a key-value secret.
+
+    Secret<SecretData> MongoSecrets = await vaultClient.V1.Secrets.KeyValue.V2.ReadSecretAsync(
+        path: "mongoSecrets",
+        mountPoint: "secret"
+    );
+
+    string? connectionString = MongoSecrets.Data.Data["ConnectionString"].ToString();
+    logger.Info($"Connection String: {connectionString}");
+
+    Environment secrets = new Environment
+    {
+        dictionary = new Dictionary<string, string> { { "ConnectionString", connectionString } }
+    };
+
     var builder = WebApplication.CreateBuilder(args);
+
+    builder.Services.AddSingleton<Environment>(secrets);
 
     // Add services to the container.
     builder.Services.AddHttpClient();
